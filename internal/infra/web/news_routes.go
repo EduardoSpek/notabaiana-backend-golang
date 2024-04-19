e5f99015d1b6833b9d3187921f54702db124be60
eduardospek <spektogran@gmail.com>
2024-04-19 00:20:20 -0300

    fix: Route Truncate News

--- internal/infra/web/news_routes.go
+++ internal/infra/web/news_routes.go
@@ -3,8 +3,8 @@ package web
 import "github.com/eduardospek/bn-api/internal/controllers"
 
 func (s *ServerWeb) NewsController(newscontroller controllers.NewsController) {
+	s.router.HandleFunc("/news/truncate/{key}", newscontroller.NewsTruncateTable).Methods("GET")
 	s.router.HandleFunc("/news", newscontroller.News).Methods("GET")
 	s.router.HandleFunc("/news/{slug}", newscontroller.GetNewsBySlug).Methods("GET")
-	s.router.HandleFunc("/news/{page}/{qtd}", newscontroller.News).Methods("GET")
-	s.router.HandleFunc("/news/truncate/{key}", newscontroller.NewsTruncateTable).Methods("GET")
+	s.router.HandleFunc("/news/{page}/{qtd}", newscontroller.News).Methods("GET")	
 }
