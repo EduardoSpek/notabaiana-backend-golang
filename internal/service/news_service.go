978c17f310e2eb2318b3587ebe66ad25460fa795
eduardospek <spektogran@gmail.com>
2024-05-03 17:15:59 -0300

    fix: ChangeWords

--- internal/service/news_service.go
+++ internal/service/news_service.go
@@ -273,7 +273,10 @@ func containsWordsInTitle(titulo string) bool {
 	palavras := []string {
 		"Bahia Notícias",
 		"BN",
-		"Curtas e Venenosas",
+		"Curtas",
+		"Nota Baiana",
+		"NotaBaiana",
+		"notabaiana",
 	}
     for _, palavra := range palavras {
         if strings.Contains(titulo, palavra) {
@@ -286,9 +289,9 @@ func changeWords(text string) string {
 	text = strings.Replace(text, "Bahia Notícias", "BN", -1)
 	text = strings.Replace(text, "@BahiaNoticias", "@", -1)
 	text = strings.Replace(text, "@bnholofote", "@", -1)
-	text = strings.Replace(text, "BN Holofote", "", -1)
+	text = strings.Replace(text, "BN Holofote", "", -1)	 
 	text = strings.Replace(text, "Siga o @bnhall_ no Instagram e fique de olho nas principais notícias.", "", -1)
-text = strings.Replace(text, "@bhall", "@", -1)
+	text = strings.Replace(text, "@bhall", "@", -1)
 	text = strings.Replace(text, "As informações são do Metrópoles, parceiro do BN", ".", -1)
 	text = strings.Replace(text, " parceiro do BN,", "", -1)
 	text = strings.Replace(text, "Assine a newsletter de Esportes do BN e fique bem informado sobre o esporte na Bahia, no Brasil e no mundo!", "", -1)
