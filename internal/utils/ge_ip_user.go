64329680cebb8c05953a6162781097b2dcef40a2
eduardospek <spektogran@gmail.com>
2024-05-25 01:09:03 -0300

    add(utils/GetIP): Adicionado funcao para obter o IP do usuario

--- internal/utils/ge_ip_user.go
+++ internal/utils/ge_ip_user.go
@@ -8,13 +8,8 @@ import (
 func GetIP(r *http.Request) string {	
 
 	// Verifica se o IP está no cabeçalho X-Forwarded-For
-	ip := r.Header.Get("X-Forwarded-For")
+	ip := r.Header.Get("X-Real-Ip")
 	
-	if ip == "" {
-		// Se não estiver, verifica o cabeçalho X-Real-Ip
-		ip = r.Header.Get("X-Real-Ip")
-	}
-
 	if ip == "" {
 		// Caso contrário, usa o IP remoto
 		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
