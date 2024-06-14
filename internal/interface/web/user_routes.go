aa2b6dcd9dee7a0fb3ccbe7bc3806284609f7a6b
Eduardo Spek <spektogran@gmail.com>
2024-06-14 03:24:52 -0300

    fix: Rota de criação de usuario desativada

--- internal/interface/web/user_routes.go
+++ internal/interface/web/user_routes.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (s *ServerWeb) UserController(usercontroller controllers.UserController) {	
-	s.router.HandleFunc("/user", usercontroller.CreateUser).Methods("POST")
+	//s.router.HandleFunc("/user", usercontroller.CreateUser).Methods("POST")
 	s.router.Handle("/user/{id}", middlewares.JwtMiddleware(http.HandlerFunc(usercontroller.UpdateUser))).Methods("PUT")
 	s.router.HandleFunc("/login", usercontroller.Login).Methods("POST")
 	s.router.HandleFunc("/accesscheck", usercontroller.AccessCheck).Methods("GET")
