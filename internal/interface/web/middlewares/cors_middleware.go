2118419c2c3a6d74df635cce4c395518d0745475
Eduardo Spek <spektogran@gmail.com>
2024-06-30 22:29:55 -0300

    Update cors_middleware.go

--- internal/interface/web/middlewares/cors_middleware.go
+++ internal/interface/web/middlewares/cors_middleware.go
@@ -7,7 +7,7 @@ import (
 // CorsMiddleware é um middleware para permitir CORS
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://notabaiana.com.br, https://us1.make.com, https://make.com")
+		w.Header().Set("Access-Control-Allow-Origin", "https://notabaiana.com.br, https://us1.make.com, https://make.com, https://n8n.cloud, https://notabaiana.app.n8n.cloud")
 		//w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
