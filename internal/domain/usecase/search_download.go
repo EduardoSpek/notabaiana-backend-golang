55a506f26722e646616a352f61df323b4c3ce22c
Eduardo Spek <spektogran@gmail.com>
2024-09-13 03:30:57 -0300

    fix: Retornanar Serach String no output

--- internal/domain/usecase/search_download.go
+++ internal/domain/usecase/search_download.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"strings"
+
 	"github.com/eduardospek/notabaiana-backend-golang/internal/domain/entity"
 	"github.com/eduardospek/notabaiana-backend-golang/internal/domain/port"
 	"github.com/eduardospek/notabaiana-backend-golang/internal/utils"
@@ -20,6 +22,8 @@ func (d *SearchDownloadUsecase) Search(page int, str_search string) (interface{}
 
 	limit := 24
 
+	str_search = strings.Replace(str_search, " ", "%", -1)
+
 	downloads, err := d.DownloadRepository.Search(page, str_search)
 
 	if err != nil {
@@ -37,9 +41,11 @@ func (d *SearchDownloadUsecase) Search(page int, str_search string) (interface{}
 	result := struct {
 		Downloads  []*entity.Download `json:"downloads"`
 		Pagination map[string][]int   `json:"pagination"`
+		Search     string             `json:"search"`
 	}{
 		Downloads:  downloads,
 		Pagination: pagination,
+		Search:     strings.Replace(str_search, "%", " ", -1),
 	}
 
 	return result, nil
