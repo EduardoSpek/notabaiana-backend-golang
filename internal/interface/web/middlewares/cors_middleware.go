4e3d37de8ea5ea528a1322f3dc12f6d45a633875
Eduardo Spek <spektogran@gmail.com>
2024-09-20 00:00:48 -0300

    fix> Cors

--- internal/interface/web/middlewares/cors_middleware.go
+++ internal/interface/web/middlewares/cors_middleware.go
@@ -7,32 +7,7 @@ import (
 // CorsMiddleware é um middleware para permitir CORS
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		allowedOrigins := []string{
-			"https://notabaiana.com.br",
-			"https://www.notabaiana.com.br",
-		}
-
-		origin := r.Header.Get("Origin")
-
-		if origin == "" {
-			origin = r.Header.Get("Referer")
-		}
-
-		allowed := false
-		for _, allowedOrigin := range allowedOrigins {
-			if origin == allowedOrigin {
-				allowed = true
-				w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
-				break
-			}
-		}
-
-		if !allowed {
-			http.Error(w, "Origem não permitida", http.StatusForbidden)
-			return
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "https://notabaiana.com.br")
 		//w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
