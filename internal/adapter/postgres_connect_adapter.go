ac02955e2f6a416b74426704c9fd3b9c57d2b717
Eduardo Spek <spektogran@gmail.com>
2024-10-02 00:31:41 -0300

    disable: Logs gorm

--- internal/adapter/postgres_connect_adapter.go
+++ internal/adapter/postgres_connect_adapter.go
@@ -2,11 +2,14 @@ package adapter
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/eduardospek/notabaiana-backend-golang/internal/domain/entity"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type PostgresAdapter struct{}
@@ -19,7 +22,17 @@ func (repo *PostgresAdapter) Connect() (*gorm.DB, error) {
 
 	connStr := "user=" + os.Getenv("POSTGRES_USERNAME") + " password=" + os.Getenv("POSTGRES_PASSWORD") + " host=" + os.Getenv("POSTGRES_HOST") + " port=" + os.Getenv("POSTGRES_PORT") + " dbname=" + os.Getenv("POSTGRES_DB") + ""
 
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Limite para considerar uma consulta lenta
+			LogLevel:                  logger.Silent, // Nível de log (Silent, Error, Warn, Info)
+			IgnoreRecordNotFoundError: true,          // Ignorar erros de "registro não encontrado"
+			Colorful:                  false,         // Desativar saída colorida
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{Logger: newLogger})
 
 	if err != nil {
 		fmt.Println(err)
