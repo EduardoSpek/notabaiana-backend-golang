7496f6288d07028ac6678e769f72bbeffca356d7
Eduardo Spek <spektogran@gmail.com>
2024-10-21 19:04:45 -0300

    fix: Access Origin Middleware

--- internal/interface/web/middlewares/access_origin_middleware.go
+++ internal/interface/web/middlewares/access_origin_middleware.go
@@ -2,44 +2,43 @@ package middlewares
 
 import (
 	"net/http"
-	"strings"
 )
 
 // CorsMiddleware é um middleware para permitir CORS
 func AccessOriginMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		allowedOrigins := []string{
-			"https://notabaiana.com.br",
-			"https://www.notabaiana.com.br",
-		}
-		origin := r.Header.Get("Origin")
-		if origin == "" {
-			origin = r.Header.Get("Referer")
-		}
+		// allowedOrigins := []string{
+		// 	"https://notabaiana.com.br",
+		// 	"https://www.notabaiana.com.br",
+		// }
+		// origin := r.Header.Get("Origin")
+		// if origin == "" {
+		// 	origin = r.Header.Get("Referer")
+		// }
+
+		// w.Header().Set("Access-Control-Allow-Origin", "*")
+		// allowed := true
+		// if origin != "" {
+		// 	for _, allowedOrigin := range allowedOrigins {
+		// 		if strings.HasPrefix(origin, allowedOrigin) {
+		// 			allowed = true
+		// 			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+		// 			break
+		// 		}
+		// 	}
+		// } else {
+		// 	// Se tanto Origin quanto Referer estiverem vazios, você pode optar por permitir a requisição
+		// 	// ou implementar uma lógica adicional de verificação
+		// 	allowed = true
+		// }
+
+		// if !allowed {
+		// 	http.Error(w, "Origem não permitida", http.StatusForbidden)
+		// 	return
+		// }
 
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		allowed := true
-		if origin != "" {
-			for _, allowedOrigin := range allowedOrigins {
-				if strings.HasPrefix(origin, allowedOrigin) {
-					allowed = true
-					w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
-					break
-				}
-			}
-		} else {
-			// Se tanto Origin quanto Referer estiverem vazios, você pode optar por permitir a requisição
-			// ou implementar uma lógica adicional de verificação
-			allowed = true
-		}
-
-		if !allowed {
-			http.Error(w, "Origem não permitida", http.StatusForbidden)
-			return
-		}
-
-		//w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 		next.ServeHTTP(w, r)
